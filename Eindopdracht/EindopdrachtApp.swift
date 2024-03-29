cf45746c9dd9b82e7a41609add5ce1e4cb2dc464
NelleFavoreel <114076571+NelleFavoreel@users.noreply.github.com>
2024-03-29 15:48:29 +0100

    Initial Commit

---
+++ Eindopdracht/EindopdrachtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EindopdrachtApp.swift
+//  Eindopdracht
+//
+//  Created by Nelle Favoreel on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct EindopdrachtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
